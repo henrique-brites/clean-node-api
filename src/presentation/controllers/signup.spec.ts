4da63545d7bbdf21592a609ed8d9197729502543
Henrique Brites <henriquebrites@live.com>
2024-06-01 19:42:03 -0300

    test: ensure SignUpController returns 200 if successful name and email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -28,4 +28,22 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
+
+  test('Should return 200 if successful name and email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'xKwJ8@example.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      name: 'any_name',
+      email: 'xKwJ8@example.com'
+    })
+  })
 })
